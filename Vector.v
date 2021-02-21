cf453e4a951e390274498a9176e2d18dd03c00d0
Adrian Lehmann <info@adrianlehmann.net>
2021-02-21 16:54:33 -0600

    Add broken zip and fold for vecs

--- Vector.v
+++ Vector.v
@@ -41,9 +41,32 @@ Definition example_vector2 : Vector nat 3 :=
 Definition length {A : Type} {n : int} : Vector A n -> int :=
   fun v => n.
 
-Theorem t2 : (length example_vector) = 3.
+Theorem t2 : (length example_vector) = (3%int63).
+Proof.
+  unfold length.
+  reflexivity.
+Qed.
 
-Show (length example_vector2).
+
+Fixpoint fold_vec {A B: Type} {n: int} (v1: Vector A n) (f: A -> B -> B) (acc: B) (idx : int) :=
+  match v1 with
+  | (vect _ a) => if eqb idx 0%int63 then f a.[idx] acc
+                 else fold_vec v1 f (f a.[idx] acc) (idx - 1%int63)
+  end.
+
+
+ 
+
+Fixpoint zip_with_vec {A B: Type} {n: int} (v1 v2: Vector A n) (f: A -> A -> B) (tgt: Vector B n) (idx: int) : Vector B n :=
+  match (v1, v2, tgt) with
+    | (vect _ a1, vect _ a2, vect _ t) =>
+      let el := f (a1.[idx]) (a2.[idx]) in
+      if eqb idx 0%int63
+      then vect _ t.[idx <- el] (* Figure out how to use length_set axiom here *)
+      else  zipWithRec v1 v2 (vect _ t.[idx <- el]) (idx - 1%int63)
+  end.
+
+
 
 Search array.
 Print array.
