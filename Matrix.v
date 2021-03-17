248d7693550153f4637cfd86024bbec8e461e540
Ben Caldwell <caldwellb@uchicago.edu>
2021-03-17 11:45:20 -0500

    Added initializers for grow_matrix_on

--- Matrix.v
+++ Matrix.v
@@ -85,10 +85,7 @@ Definition matrix_get_row {A : Type} {n m : nat} (i : int) (mat : Matrix A n m) 
 Notation "t .[ i , '_' ]" := (matrix_get_row t i)
                              (at level 2, left associativity, format "t .[ i , '_' ]").
 
-(*Definition matrix_get_col {A : Type} {n m : nat} (i : int) (mat : Matrix A n m) : Vector A n :=
-  matrix_get_row (matrix_transpose mat) i.
-Notation "t .[ '_' , i ]" := (matrix_get_col t i)
-                             (at level 2, left associativity, format "t .[ '_' , i ]"). *)
+
 
 
 Definition matrix_transpose_el_on {A : Type} {n m : nat} (i j : int) (mat: Matrix A n m) (tgt: Matrix A m n) : Matrix A m n :=
@@ -108,6 +105,14 @@ Fixpoint matrix_transpose_on_helper {A : Type} {n m: nat} (nj : nat) (i j : int)
 
 Definition matrix_transpose_on {A : Type} {n m : nat} (mat : Matrix A n m) (tgt : Matrix A m n) :=
   matrix_transpose_on_helper (m - 1) ((vect_length_int mat) - 1%int63) ((vect_length_int (mat.[0%int63] (* Maybe replace with default *))) - 1%int63) mat tgt.
+
+Definition matrix_transpose {A : Type} {n m : nat} (mat : Matrix A n m) :=
+  matrix_transpose_on mat (transpose_on_init mat).
+
+Definition matrix_get_col {A : Type} {n m : nat} (i : int) (mat : Matrix A n m) : Vector A n :=
+  matrix_get_row i (matrix_transpose mat).
+Notation "t .[ '_' , i ]" := (matrix_get_col t i)
+                             (at level 2, left associativity, format "t .[ '_' , i ]"). 
 
 Fixpoint matrix_mult_single_rc_helper {A : Type} `{F : Field A} {n m l : nat} (ni : nat) (i c1 c2: int) (acc : A) (mat1 : Matrix A n m) (mat2 : Matrix A m l) : A :=
   match ni with
@@ -181,22 +186,58 @@ Fixpoint grow_vector_on_helper  {A : Type} {n : nat}
   | S k => grow_vector_on_helper v (tgt.[i+1%int63 <- v.[i]]) (k) (i-1%int63)
   end.
 
+Fixpoint nat_int (n : nat) : int :=
+  match n with
+  | 0 => 0%int63
+  | S k => 1%int63 + (nat_int k)
+  end.
 
+
+Lemma intgez_gez_circle : forall (k : nat) (i : int), k = BinInt.Z.to_nat(to_Z(i)) -> nat_int k = i.
+Proof.
+  induction k.
+  intros.
+  remember (to_Z i) as Zi.
+  induction Zi.
+  - simpl in H.
+    Search to_Z.
+    rewrite <- to_Z_0 in HeqZi.
+    simpl.
+    Search to_Z.
+    rewrite <- (of_to_Z 0).
+    rewrite HeqZi.
+    rewrite (of_to_Z i).
+    reflexivity.
+  - simpl.
+    Search to_Z.
+    rewrite <- (of_to_Z i).
+    rewrite <- HeqZi.
+    simpl.
+    Admitted.
+        
+    
+  
 
 
 Theorem shave_vec_init : forall {A : Type} {n : nat} (v : Vector A (S n)), Vector A n.
   intros.
+  Search (nat -> int).
+  Search (int -> int).
   apply vect with (make
-                     (vect_length_int v - 1)
+                     (pred (vect_length_int v))
                      (vect_default v)).
   rewrite length_make.
-  rewrite leb_trans with (sub (vect_length_int v) 1%int63) (vect_length_int v) (max_length).
+  rewrite leb_trans with (pred (vect_length_int v)) (vect_length_int v) (max_length).
   - remember (S n) as k.
     destruct v eqn:E.
-    simpl.
     subst.
-    Admitted.
+    induction n.
+    + simpl.
+      Search length.
+      Admitted.
 
+
+                                                         
 
 Definition grow_vector_on {A : Type} {n : nat} (v : Vector A n) (a : A) (tgt : Vector A (n + 1)) :=
   grow_vector_on_helper v (tgt.[0%int63 <- a]) n (vect_length_int v - 1%int63).
@@ -208,8 +249,21 @@ Fixpoint grow_matrix_on_helper {A : Type} {n m : nat} (ni : nat) (i : int) (top_
     | S ni' => grow_matrix_on_helper ni' (i - 1%int63) top_vec mat new_tgt
     end.
 
+Lemma grow_matrix_on_helper_lemma : forall {A : Type} {n m : nat}
+                                      (v : Vector A m)
+                                      (mat : Matrix A (n + 1) (m + 1)), (Matrix A (n + 1) m).
+Proof.
+  intros.
+  apply vect with (make
+                     (vect_length_int mat)
+                     (v)).
+  rewrite length_make.
+  rewrite vect_leb_length.
+  destruct mat.
+  assumption.
+Qed.
+
 Definition grow_matrix_on {A : Type} {n m : nat} (mat : Matrix A n m)
            (top_vec : Vector A n) (left_vec : Vector A m) (corner_el : A)
            (tgt : Matrix A (n+1) (m+1)) : Matrix A (n + 1) (m + 1) :=
-  grow_matrix_on_helper ((n + 1) - 1) ((vect_length_int tgt) - 1%int63)  (grow_vector_on top_vec corner_el _) (grow_vector_on mat left_vec _) tgt.
-
+  grow_matrix_on_helper ((n + 1) - 1) ((vect_length_int tgt) - 1%int63)  (grow_vector_on top_vec corner_el (transpose_on_lemma tgt)) (grow_vector_on mat left_vec (grow_matrix_on_helper_lemma left_vec tgt)) tgt.
